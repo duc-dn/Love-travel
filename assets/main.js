bce11f121e44bca5f44802f8c9cfa94404992e58
duc-dn <ducdo23022001@gmail.com>
2021-08-24 15:39:39 +0700

    Fixed pc-menu and best

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,12 @@
-let header = document.querySelector('.header')
-let btnClose = document.getElementById('btn-close')
-let pcMenu = document.getElementById('pc-menu')
-console.log(pcMenu)
-let pcMenuContainer = document.getElementById('pc-menu__container')
-let menu = document.getElementById('menu')
+const header = document.querySelector('.header')
+const btnClose = document.getElementById('btn-close')
+const pcMenuOverlay = document.getElementById('pc-menu__overlay')
+
+const pcMenuContainer = document.getElementById('pc-menu__container')
+const menu = document.getElementById('menu')
+
+
+// Xử lý navabar khi cuộn thì dính lên trên
 window.addEventListener("scroll", (e) => {
     let y = pageYOffset;
     
@@ -15,18 +18,22 @@ window.addEventListener("scroll", (e) => {
 })
 
 
+// menu trên pc
 function showPcMenu() {
-    pcMenu.style.cssText = 'display: block; opacity: 1'
-    pcMenuContainer.style.transform = 'translateX: 0'
+    pcMenuOverlay.style.cssText = 'opacity: 1; display: block'
+    pcMenuContainer.style.cssText = 'transform: translateX(0); opacity: 1'
 }
 menu.addEventListener('click', showPcMenu) 
 
+
 function hidePcMenu() {
-    pcMenu.style.cssText = 'display: none;'
+    pcMenuContainer.style.cssText = 'transform: translateX(105%);'
+    pcMenuOverlay.style.cssText = 'opacity: 0; display: none'
 }
 btnClose.addEventListener('click', hidePcMenu)
+pcMenuOverlay.addEventListener('click', hidePcMenu)
 
-pcMenu.addEventListener('click', hidePcMenu)
+
 pcMenuContainer.addEventListener("click", function(e) {
     e.stopPropagation();
 })
